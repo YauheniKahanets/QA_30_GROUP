db7221ab29da7fdae693e8d7f356e5ab8cc5d4c0
YauheniKahanets <yauheni.kahanets@gmail.com>
2022-06-26 23:39:39 +0300

    Upd. Bug fixes

--- JS/Jekich_JS_HW_1*.js
+++ JS/Jekich_JS_HW_1*.js
@@ -5,17 +5,22 @@
 }
 Вывести в консоль результат работы функции с возрастами 17, 18, 61 
 */
+let age_2 = 18
+let age_3 = 60
 const checkAge = function(age) {
     if (age){
-        if (age < 18) {
+        if (age < age_2) {
             console.log("You don’t have access cause your age is " + age + " It’s less then ")}
-        else if ((age >= 18) && (age < 60)) {
+        else if ((age >= age_2) && (age <= age_3)) {
             console.log("Welcome !")}
-        else if (age > 60){
+        else if (age > age_3){
             console.log("Keep calm and look Culture channel")}
         else {
             console.log("Technical work")}
-    }   
+    }
+    else {
+        console.log("Technical work")
+    }  
 }
 checkAge(17)
 checkAge(18)
@@ -27,18 +32,18 @@ checkAge(61)
 чтобы первым делом в функции проверялся тип данных. 
 И если он не Number - кидалась ошибка.
 */
+let age_2 = 18
+let age_3 = 60
 const checkAge = function(age) {
-    if (typeof age == 'number') {
-        if (age){
-            if (age < 18) {
-                console.log("You don’t have access cause your age is " + age + " It’s less then ")}
-            else if ((age >= 18) && (age < 60)) {
-                console.log("Welcome !")}
-            else if (age > 60){
-                console.log("Keep calm and look Culture channel")}
-            else {
-                console.log("Technical work")}
-        }
+    if ((typeof age == 'number') && (age)){
+        if (age < age_2) {
+            console.log("You don’t have access cause your age is " + age + " It’s less then ")}
+        else if ((age >= age_2) && (age <= age_3)) {
+            console.log("Welcome !")}
+        else if (age > age_3){
+            console.log("Keep calm and look Culture channel")}
+        else {
+            console.log("Technical work")} 
     }
     else {
         throw TypeError('Not number')} 
@@ -53,19 +58,18 @@ checkAge(61)
 чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, 
 преобразовываясь в number
 */
+let age_2 = 18
+let age_3 = 60
 const checkAge = function(age) {
-    if ((parseInt(age)) || (typeof age == 'number')) {
-
-        if (age){
-            if (age < 18) {
-                console.log("You don’t have access cause your age is " + age + " It’s less then ")}
-            else if ((age >= 18) && (age < 60)) {
-                console.log("Welcome !")}
-            else if (age > 60){
-                console.log("Keep calm and look Culture channel")}
-            else {
-                console.log("Technical work")}
-        }
+    if (!Number.isFinite(age)){
+        if (age < age_2) {
+            console.log("You don’t have access cause your age is " + age + " It’s less then ")}
+        else if ((age >= age_2) && (age <= age_3)) {
+            console.log("Welcome !")}
+        else if (age > age_3){
+            console.log("Keep calm and look Culture channel")}
+        else {
+            console.log("Technical work")} 
     }
     else {
         throw TypeError('Not number')} 
@@ -79,19 +83,20 @@ checkAge(61)
 Преобразовать задание 3* таким образом, 
 чтобы возраст вводится используя функцию prompt в привязанной верстке
 */
+let age_2 = 18
+let age_3 = 60
 var age = prompt('How old are u?', '')
 const checkAge = function(age) {
-    if ((parseInt(age)) || (typeof age == 'number')) { 
-        if (age){
-            if (age < 18) {
-                console.log("You don’t have access cause your age is " + age + " It’s less then ")}
-            else if ((age >= 18) && (age < 60)) {
-                console.log("Welcome !")}
-            else if (age > 60){
-                console.log("Keep calm and look Culture channel")}
-            else {
-                console.log("Technical work")}
-        }
+    if (!Number.isFinite(age)){
+        age = isFinite(age)
+        if (age < age_2) {
+            console.log("You don’t have access cause your age is " + age + " It’s less then ")}
+        else if ((age >= age_2) && (age <= age_3)) {
+            console.log("Welcome !")}
+        else if (age > age_3){
+            console.log("Keep calm and look Culture channel")}
+        else {
+            console.log("Technical work")} 
     }
     else {
         throw TypeError('Not number')} 
